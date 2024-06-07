b25811521852770e0f6c33c5083479492cebba2e
corelabui <146253277+corelabui@users.noreply.github.com>
2024-06-07 12:29:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite + Formfusion
+
+This template contains a minimal example of a simple form built using FormFusion to showcase its accessibility features.
---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { Form, Input } from 'formfusion';
+import './App.css';
+
+function App() {
+  return (
+    <Form validateOnChange>
+      <fieldset>
+        <legend>Personal Information</legend>
+        <Input
+          type="alphabetic"
+          id="firstName"
+          name="firstName"
+          label="First name"
+          classes={{ root: 'formControl', error: 'formControl__error' }}
+        />
+        <Input
+          type="alphabetic"
+          id="lastName"
+          name="lastName"
+          label="Last name"
+          classes={{ root: 'formControl', error: 'formControl__error' }}
+        />
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          label="Email"
+          required
+          classes={{ root: 'formControl', error: 'formControl__error' }}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Address</legend>
+        <Input
+          type="text"
+          id="street"
+          name="street"
+          label="Street"
+          classes={{ root: 'formControl', error: 'formControl__error' }}
+        />
+        <Input
+          type="alphabetic"
+          id="city"
+          name="city"
+          label="city"
+          classes={{ root: 'formControl', error: 'formControl__error' }}
+        />
+      </fieldset>
+      <button type="submit">Save</button>
+    </Form>
+  );
+}
+
+export default App;
